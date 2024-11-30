51ed9c0c5faaf6d17bddaee0e31811c75316849f
bethCoderNewbie <155182607+bethCoderNewbie@users.noreply.github.com>
2024-11-30 09:15:06 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-
-export const ArrowUp = ({ className, strokeWidth, color }) => (
+const ArrowUp = ({ className, strokeWidth, color }) => (
   <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
     <path d="M12 19V5M5 12l7-7 7 7"/>
   </svg>
 );
 
-export const ArrowRight = ({ className, strokeWidth, color }) => (
+const ArrowRight = ({ className, strokeWidth, color }) => (
   <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
     <path d="M5 12h14M12 5l7 7-7 7"/>
   </svg>
 );
 
-export const ArrowDown = ({ className, strokeWidth, color }) => (
+const ArrowDown = ({ className, strokeWidth, color }) => (
   <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
     <path d="M12 5v14M5 12l7 7 7-7"/>
   </svg>
 );
 
-export const ArrowLeft = ({ className, strokeWidth, color }) => (
+const ArrowLeft = ({ className, strokeWidth, color }) => (
   <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
     <path d="M19 12H5M12 19l-7-7 7-7"/>
   </svg>
 );
+
+export { ArrowUp, ArrowRight, ArrowDown, ArrowLeft };
