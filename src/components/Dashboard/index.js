0d94fc5f7bf1b587349e82ca7e39eb23bd0ad17f
bethCoderNewbie <155182607+bethCoderNewbie@users.noreply.github.com>
2024-11-30 11:18:14 +0700

    Update index.js

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,3 +2,4 @@ export { default as StrategicGrowthMatrix } from './StrategicGrowthMatrix';
 export { default as UncertaintyMatrix } from './UncertaintyMatrix';
 export { default as ScenarioPlanning } from './ScenarioPlanning';
 export { default as PestelAnalysis } from './PestelAnalysis';
+export { default as Presentation } from './Presentation';
