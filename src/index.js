a616e82373c807364934153ded9b1bb036399b23
bethCoderNewbie <155182607+bethCoderNewbie@users.noreply.github.com>
2024-11-30 09:30:55 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,2 @@
-const ArrowUp = ({ className, strokeWidth, color }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
-    <path d="M12 19V5M5 12l7-7 7 7"/>
-  </svg>
-);
-
-const ArrowRight = ({ className, strokeWidth, color }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
-    <path d="M5 12h14M12 5l7 7-7 7"/>
-  </svg>
-);
-
-const ArrowDown = ({ className, strokeWidth, color }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
-    <path d="M12 5v14M5 12l7 7 7-7"/>
-  </svg>
-);
-
-const ArrowLeft = ({ className, strokeWidth, color }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
-    <path d="M19 12H5M12 19l-7-7 7-7"/>
-  </svg>
-);
-
-export { ArrowUp, ArrowRight, ArrowDown, ArrowLeft };
+export { StrategicGrowthMatrix } from './StrategicGrowthMatrix';
+export { UncertaintyMatrix } from './UncertaintyMatrix';
