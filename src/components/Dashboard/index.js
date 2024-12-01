ab81e385a38ee70a15170ccaa7536124b56e623a
bethCoderNewbie <155182607+bethCoderNewbie@users.noreply.github.com>
2024-12-01 09:50:41 +0700

    Update index.js

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -3,3 +3,4 @@ export { default as UncertaintyMatrix } from './UncertaintyMatrix';
 export { default as ScenarioPlanning } from './ScenarioPlanning';
 export { default as PestelAnalysis } from './PestelAnalysis';
 export { default as Presentation } from './Presentation';
+export { default as AIIntegrationDashboard } from './AIIntegrationDashboard;
